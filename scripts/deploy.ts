3dfae6df2117a872259fe9cd746a1798cf7d7043
Fran Rimoldi <francisco.rimoldi@gmail.com>
2022-06-09 16:45:10 +0200

    feat: add ERC20 NDToken contract

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -11,11 +11,16 @@ async function main() {
   const Token = await ethers.getContractFactory("Token")
   const token = await Token.deploy()
 
+  const NDToken = await ethers.getContractFactory("NDToken")
+  const ndToken = await NDToken.deploy("Nader Dabit Token", "NDT")
+
   await greeter.deployed()
   await token.deployed()
+  await ndToken.deployed()
 
-  console.log("Greeter deployed to:", greeter.address)
-  console.log("Token deployed to:", token.address)
+  console.log("Greeter deployed to: ", greeter.address)
+  console.log("Token deployed to: ", token.address)
+  console.log("NDToken deployed to: ", ndToken.address)
 }
 
 main()
