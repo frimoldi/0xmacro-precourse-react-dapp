c104511b5e2347e026546a33fb952d2353e124bb
Fran Rimoldi <francisco.rimoldi@gmail.com>
2022-06-09 15:29:47 +0200

    feat: fetch and set greeting

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+import { Button, Input, VStack } from "@chakra-ui/react"
+import "./App.css"
+
+import Greeter from "./artifacts/contracts/Greeter.sol/Greeter.json"
+import { ethers } from "ethers"
+
+const GREETER_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
 
 function App() {
+  const [greeting, setGreetingValue] = useState<string>()
+
+  async function requestAccount() {
+    await window.ethereum.request({ method: "eth_requestAccounts" })
+  }
+
+  async function fetchGreeting() {
+    if (typeof window.ethereum !== "undefined") {
+      const provider = new ethers.providers.Web3Provider(window.ethereum)
+      const contract = new ethers.Contract(
+        GREETER_ADDRESS,
+        Greeter.abi,
+        provider
+      )
+      try {
+        const data = await contract.greet()
+        console.log("data: ", data)
+      } catch (err) {
+        console.log("Error: ", err)
+      }
+    }
+  }
+
+  async function setGreeting() {
+    if (!greeting) return
+    if (typeof window.ethereum !== "undefined") {
+      await requestAccount()
+      const provider = new ethers.providers.Web3Provider(window.ethereum)
+      const signer = provider.getSigner()
+      const contract = new ethers.Contract(GREETER_ADDRESS, Greeter.abi, signer)
+      const transaction = await contract.setGreeting(greeting)
+      await transaction.wait()
+      fetchGreeting()
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <VStack height="100vh" justifyContent="center" alignItems="center">
+      <VStack style={{ width: "800px" }} spacing="5">
+        <Button onClick={fetchGreeting}>Fetch Greeting</Button>
+        <Button onClick={setGreeting}>Set Greeting</Button>
+        <Input
+          onChange={(e) => setGreetingValue(e.target.value)}
+          placeholder="Set greeting"
+        />
+      </VStack>
+    </VStack>
+  )
 }
 
-export default App;
+export default App
